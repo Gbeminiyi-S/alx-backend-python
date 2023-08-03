be7c97e6dafa53883b8d1772a492deadcb95735d
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-08-03 09:12:38 +0000

    Task 2 solution

---
+++ 2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines a function `floor`"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """returns the floor value of float n"""
+    return math.floor(n)
