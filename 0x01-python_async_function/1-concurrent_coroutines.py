f5726ed62a05e8966f00d597d7cc95779b63d716
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-08-07 11:05:39 +0000

    Task 1 - pycodestyle adjustment

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,13 +3,13 @@
 import asyncio
 import random
 
-
 wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 async def wait_n(n: int, max_delay: int) -> list[float]:
     """spawn wait_random n times with the specified max_delay"""
     tasks = [wait_random(max_delay) for _ in range(n)]
 
     results = await asyncio.gather(*tasks)
-    
+
     return sorted(results)
