c15ae3fe2e48e4bd7846dc0b93010125996242cb
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-08-30 15:56:31 +0000

    Task 1 - solution

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -19,3 +19,13 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nestedMap, path, expectedResult):
         """ Test the access_nested_map function with diff. input scenarios """
         self.assertEqual(access_nested_map(nestedMap, path), expectedResult)
+
+    @parameterized.expand([
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """ Test that a KeyError is raised for diff. inputs """
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, path)
+        self.assertEqual(f"KeyError('{expected}')", repr(e.exception))
