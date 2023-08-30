21f140e390a5bd42f0593975f71797b4778d55a9
Gbeminiyi <samagbeyigbeminiyi@gmail.com>
2023-08-30 15:33:18 +0000

    corrected parameters

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ This module tests the utils package """
 
-from parameterized import parameterized
 import unittest
+from parameterized import parameterized
 from utils import access_nested_map
 
 
@@ -13,8 +13,8 @@ class TestAccessNestedMap(unittest.TestCase):
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), 2),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nestedMap, path, expectedResult):
         """ Test the access_nested_map function with diff. input scenarios """
